a6a49cdc10e454901f2c7d97bc2236d94838c544
Lyndon Coghill <lcoghill@fieldmuseum.org>
2014-01-31 12:25:14 -0600

    Added initial functionality to pull sequences, GI values and descriptions from the Phylota DB, and then parse those values and convert them to a single FASTA formatted entry to easy output to a FASTA file.

--- get-clusters.py
+++ get-clusters.py
@@ -2,6 +2,8 @@ import MySQLdb as mdb
 import sys
 import os
 import logging
+import pickle
+
 
 ### Read in a parse the tree file output from 'phylota-graph'
 
@@ -16,40 +18,51 @@ for line in treeFile:
     label2[0] = label2[0][2:]
     label2[1] = label2[1][2:]
     ci_ti.append(label2)
-    #ci_ti[1] = ci_ti[1][2:]
-    #ci.append (ci_ti[1]) #create a list of ci values
+ 
 
-    
-print ci_ti
-
+### To pull sequences from phylota mySQL db based on the ti and ci information retreived from the tree file    
 config = [];
-db_params = open('private/config', 'r')
+db_params = open('private/config', 'r') ## define a config file so that private db information isn't shared across code repositories. to create, renamed /private/config.example to /private/config
 
 for line in db_params:
 	line = line.rstrip('\n')
 	line = line.split("=", 1)
 	config.append (line[1])
 
-#print config
-
 database = mdb.connect(host=config[1], # your host, usually localhost
                      user=config[2], # your username
                      passwd=config[3], # your password
                      db=config[0]) # name of the data base
 
-# you must create a Cursor object. It will let
-# you execute all the query you need
-cluster_db = database.cursor()
 
+cluster_db = database.cursor() # define the db cursor
 
-# Use all the SQL you like
 ti_list = []
 count = 0
 for cluster in ci_ti:
+    #sql query to find the sequences that belong to the cluster / taxon that are present in the given tree
+    #sql = "".join(["SELECT seqs.gi,seqs.seq,seqs.def FROM seqs LEFT JOIN ci_gi_184 ON seqs.gi=ci_gi_184.gi WHERE ci_gi_184.ti=2763"," AND ci_gi_184.clustid=505 AND ci_gi_184.cl_type='subtree';"])
     sql = "".join(["SELECT seqs.gi,seqs.seq,seqs.def FROM seqs LEFT JOIN ci_gi_184 ON seqs.gi=ci_gi_184.gi WHERE ci_gi_184.ti=", ci_ti[count][0]," AND ci_gi_184.clustid=",ci_ti[count][1], " AND ci_gi_184.cl_type='subtree';"])
-    #print sql
-    cluster_db.execute(sql)
-    ti_list.append(cluster_db.fetchall())
-    count = count + 1
+    cluster_db.execute(sql) #execute the above sql query
+    record = cluster_db.fetchall() #fetch all records that meet the query criteria and place them in the list record
+    ti_list.append(record) #append the list 'record' to the tuple ti_list
+    filename = "".join(["fasta/ti",ci_ti[count][0],"_ci",ci_ti[count][1],".FASTA"]) # create a string that contains the appropriate filename for each FASTA file
+  
 
-print ti_list
+    counter = 0
+    for r in ti_list:
+        cur_record = str(ti_list[counter][0]) #convert the active element of the tuple ti_list to a string, and assing it to cur_record
+        cur_record = cur_record.split(",") #split the cur_record string on the commas and reassign to cur_record which becomes a list
+        gi = cur_record.pop(0) #pull out the gi value
+        gi = gi[1:].replace("L","") #strip a leading '(' from the gi value
+        seq = cur_record.pop(0).replace('\'',"")[1:]  #pull out the actual sequence, only leaving behind the elements of the description
+        description = "".join(cur_record).replace('\'', "").strip(")")[1:] #join all the pieces of the description into a single string and remove problem characters and a leading space
+        fasta_entry = "".join([">gi|",gi,"|",description,"\n",seq]) #parse and format strings into a single string fasta entry
+        counter = counter + 1
+    
+    print str(count) + ": " + gi + " successful"
+    count = count + 1
+    
+    
+
+    
