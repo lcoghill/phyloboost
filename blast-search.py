f64d7cab9cabd3b510d358aff24b2327e1608fe9
Lyndon Coghill <lcoghill@fieldmuseum.org>
2014-02-01 19:00:39 -0600

    Fixed a bug in character removal from the "f" variable. It was removing 1 too many characters breaking the file name for other scripts.

--- blast-search.py
+++ blast-search.py
@@ -22,21 +22,21 @@ print "There are %s FASTA files to be processed." %file_count
 print "This process can take quite a long time.\n\n"
 
 for f in fasta_files:
-	print "Reading original FASTA file %s" %f[7:]
+	print "Reading original FASTA file %s" %f[6:]
 	with open (f, "r") as fasta_file:
 		## parse input to get a clean file_id
 		sequences = fasta_file.read()
 		fasta_file.close()
 		print "Done."
 
-		print "Starting BLASTN Search against %s" %f[7:]
+		print "Starting BLASTN Search against %s" %f[6:]
 		print "This may take awhile, and requires a presistant Internet connection..."
 		## Blast using the FASTA set, and assing the XML string result to result_handle
 		result_handle = NCBIWWW.qblast("blastn", "nt", sequences)
 		print "Done."
 		print "Writing results to a BLAST.XML file..."
 		## write the XML string to a file for later parsing
-		blast_file_id = "".join(["blast-temp/", f[7:-6], ".xml"])
+		blast_file_id = "".join(["blast-temp/", f[6:-6], ".xml"])
 		save_result = open(blast_file_id, "w")
 		save_result.write(result_handle.read())
 		save_result.close()
