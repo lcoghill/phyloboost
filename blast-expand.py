77715b156afb2b1afa35f7f40c5a0fb8a8f40c1d
Lyndon Coghill <lcoghill@fieldmuseum.org>
2014-02-01 18:59:51 -0600

    Fixed bugs related to file import caused from splitting blast-search and blast-expand into individual scripts.

--- blast-expand.py
+++ blast-expand.py
@@ -17,16 +17,17 @@ library.genbank.email = 'me@my.address.com'
 
 ### Functions
 
-def fasta_process(file_name):
+def fasta_process(file_name, blast_file):
 	with open (file_name, "r") as fasta_file:
 		## parse input to get a clean file_id
 		file_id = file_name.split("/")
 		file_id = file_id[1].split(".")
 		file_id = file_id[0]
+		
 
 	### Parses the XML Blast return document, and returns a clean list of only gi (genebank ids) for the sequences of interest
 	print "Parsing BLAST Results file for GI values..."
-	for record in NCBIXML.parse(open(blast_file_id)) :
+	for record in NCBIXML.parse(open(blast_file)) :
 	    #We want to ignore any queries with no search results:
 	    gi_list = []
 	    if record.alignments :
@@ -46,6 +47,12 @@ def fasta_process(file_name):
 
 
 	### Use the list of gi values to retreive the appropriate genbank FASTA data for those records
+	print "Getting original sequences from FASTA file %s" %file_id[6:]
+	with open (f, "r") as fasta_file:
+		## parse input to get a clean file_id
+		sequences = fasta_file.read()
+		fasta_file.close()
+		print "Done."
 	print "Grabbing genbank data for new GIs..."
 	new_fasta_file_name = "".join(["fasta/expanded-fasta/",file_id,".FASTA"])
 	new_fasta_file = open(new_fasta_file_name, "w+")
@@ -62,17 +69,17 @@ def fasta_process(file_name):
 ### Program Code
 
 fasta_files = glob.glob("fasta/*.FASTA") # get a list of all fasta files in /fasta
+blast_files = glob.glob("blast-temp/*.xml") # get a list of all fasta files in /fasta
 
+count = 0
 for f in fasta_files:
 	## process each file by calling fasta_process
-	fasta_process(f)
+	fasta_process(f, blast_files[count])
 	print "FASTA file %s successfully expanded." % f
+	count = count + 1
 
-print "Cleaning up undeed files..."
-## remove uneeded BLAST XML Result Files
 
 print "Done."
-
 print "Adding additional records to FASTA complete."
 print "New FASTA files can be found in /fasta/expanded-fasta directory."
 
