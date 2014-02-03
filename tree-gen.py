fba6a2e21a3c7e16fd5e9989b9c368c6c0323f53
Lyndon Coghill <lcoghill@fieldmuseum.org>
2014-02-03 15:13:51 -0600

    Switched to the AlignIO.read function for more compatibility. Now correctly reads in the alignment file under strict clustalw formatting.

--- tree-gen.py
+++ tree-gen.py
@@ -1,17 +1,10 @@
 from Bio import SeqIO
 from Bio.Phylo.Applications import RaxmlCommandline
 from StringIO import StringIO
+from Bio import AlignIO
 
-input_handle = open("alignments/testfile.align.FASTA", "rU")
-output_handle = open("testfile.nex", "w")
-
-for record in SeqIO.parse(input_handle, "fasta") :
-	sequences = SeqIO.parse(input_handle, "fasta")
-	print record.id
-	SeqIO.write(sequences, output_handle, "nexus")
-
-output_handle.close()
-handle.close()
+align = AlignIO.read("alignments/testfile.align", "clustal")
+print align
 
 #raxml_cline = RaxmlCommandline(sequences="testfile.nex", model="PROTCATWAG", name="interlaced2")
 #stdout, stderr = raxml_cline()
