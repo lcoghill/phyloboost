c7b2e86ac9502f1bfc2fdf7d1024aa0ebea78cf4
Lyndon Coghill <lcoghill@fieldmuseum.org>
2014-02-03 14:03:01 -0600

    Initial commit. Basic working copy of script to read in all of the new FASTA files generated earlier in the pipeline, and create Muscle alignments for each one.

---
+++ muscle-alignment.py
@@ -0,0 +1,22 @@
+from Bio.Align.Applications import MuscleCommandline
+from StringIO import StringIO
+from Bio import AlignIO
+import glob
+
+##get list of all new fasta files
+
+print "\n\nGetting a list of FASTA files..."
+fasta_files = glob.glob("fasta/expanded-fasta/*.FASTA") # get a list of all fasta files in /fasta
+file_count = len(fasta_files)
+print "%s files successful found.\n" %file_count
+
+
+### iterate through each file doing the following:
+
+for f in fasta_files: 
+	print "Aligning FASTA file %s" % f
+	file_name = f[21:-6]
+	alignment = MuscleCommandline(input=f, out="".join([file_name,"align.FASTA"]))
+	stdout, stderr = alignment()
+	print "Success."
+
