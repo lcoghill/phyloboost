1fdf23d6ef94e2eb6d9941a0d4645c96907043c4
Lyndon Coghill <lcoghill@fieldmuseum.org>
2014-02-04 15:21:05 -0600

    Fixed a major bug where the appropriate records were not being retreived from the Phylota DB due to a failed iteration through the DB query result tuple. Appropriately returns the correct number of records formatted exactly like the Phylota.net online DB FASTA files.

--- get-clusters.py
+++ get-clusters.py
@@ -3,6 +3,7 @@ import sys
 import os
 import logging
 import pickle
+import itertools
 
 
 ### Read in a parse the tree file output from 'phylota-graph'
@@ -17,8 +18,12 @@ for line in treeFile:
     label2 = label[0].split('_', 1 ); #split the label into ti and ci
     label2[0] = label2[0][2:]
     label2[1] = label2[1][2:]
+    label3 = label[1].split('\t')
+    label3[1] = label3[1].replace(" ","_")
+    label2.extend(label3)
+    label2 = label2[:-1]
     ci_ti.append(label2)
- 
+    
 
 ### To pull sequences from phylota mySQL db based on the ti and ci information retreived from the tree file    
 config = [];
@@ -37,32 +42,24 @@ database = mdb.connect(host=config[1], # your host, usually localhost
 
 cluster_db = database.cursor() # define the db cursor
 
-ti_list = []
 count = 0
 for cluster in ci_ti:
     #sql query to find the sequences that belong to the cluster / taxon that are present in the given tree
     sql = "".join(["SELECT seqs.gi,seqs.seq,seqs.def FROM seqs LEFT JOIN ci_gi_184 ON seqs.gi=ci_gi_184.gi WHERE ci_gi_184.ti=", ci_ti[count][0]," AND ci_gi_184.clustid=",ci_ti[count][1], " AND ci_gi_184.cl_type='subtree';"])
     cluster_db.execute(sql) #execute the above sql query
     record = cluster_db.fetchall() #fetch all records that meet the query criteria and place them in the list record
-    ti_list.append(record) #append the list 'record' to the tuple ti_list
-    filename = "".join(["fasta/ti",ci_ti[count][0],"_ci",ci_ti[count][1],".FASTA"]) # create a string that contains the appropriate filename for each FASTA file
+    record = list(record) #convert tuple to list
+    filename = "".join(["fasta/ti",ci_ti[count][0],"_ci",ci_ti[count][1],"_",ci_ti[count][2],".FASTA"]) # create a string that contains the appropriate filename for each FASTA file
     f = open(filename,'w+')
-
-    counter = 0
-    for r in ti_list:
-        cur_record = str(ti_list[counter][0]) #convert the active element of the tuple ti_list to a string, and assing it to cur_record
-        cur_record = cur_record.split(",") #split the cur_record string on the commas and reassign to cur_record which becomes a list
-        gi = cur_record.pop(0) #pull out the gi value
-        gi = gi[1:].replace("L","") #strip a leading '(' from the gi value
-        seq = cur_record.pop(0).replace('\'',"")[1:]  #pull out the actual sequence, only leaving behind the elements of the description
-        description = "".join(cur_record).replace('\'', "").strip(")")[1:] #join all the pieces of the description into a single string and remove problem characters and a leading space
-        fasta_entry = "".join([">gi|",gi,"|",description,"\n",seq,"\n"]) #parse and format strings into a single string fasta entry
-        counter = counter + 1
-        f.write(fasta_entry)
-        
+    
+    for r in record:
+        cur_record = list(r) #convert this element of the record list from tuple to a list
+        cur_record[0] = str(cur_record[0]) #convert the GID value from long to string
+        cur_record = "".join([">gi|",cur_record[0],"|",cur_record[2],"\n",cur_record[1]]) #join all elements of the cur_record list into a single string, added formatting for FASTA style
+        f.write("%s\n" % cur_record)    
     
     print "Sequences for tree " + str(count) + " successfully written to " + filename + "."
     count = count + 1
-    
+    f.close()
 
     
