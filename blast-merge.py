fce905ae3b7fd89891111fa92cb9de663c27fa6b
Lyndon Coghill <lcoghill@fieldmuseum.org>
2014-02-05 16:34:45 -0600

    Added Snapshot Stats for files / sequences processed. Also fixed a bug in sequence output to FASTA from original FASTA file.

--- blast-merge.py
+++ blast-merge.py
@@ -13,11 +13,17 @@ from Bio.Blast import NCBIWWW, NCBIXML
 
 ### A few import static variables
 library.genbank.email = 'me@my.address.com'    
-
-
 ### Functions ###
 
+new_seqs = 0;
+total_new_seqs = 0;
+total_orig_seqs = 0;
+orig_seqs = 0;
+
 def fasta_process(file_name, blast_file):
+	global new_seqs
+	global orig_seqs
+
 	with open (file_name, "r") as fasta_file:
 		## parse input to get a clean file_id
 		file_id = file_name.split("/")
@@ -47,39 +53,52 @@ def fasta_process(file_name, blast_file):
 
 
 	### Use the list of gi values to retreive the appropriate genbank FASTA data for those records
-	print "Getting original sequences from FASTA file %s" %file_id[4:]
+	print "Getting original sequences from FASTA file %s" %file_id[2:]
 	with open (f, "r") as fasta_file:
-		## parse input to get a clean file_id
-		sequences = fasta_file.read()
+		## parse input to get a clean file_id		
+		sequences = fasta_file.readlines()
+		orig_seqs = orig_seqs + len(sequences)
 		fasta_file.close()
 		print "Done."
 	print "Grabbing genbank data for new GIs..."
 	new_fasta_file_name = "".join(["fasta/expanded-fasta/",file_id,".FASTA"])
 	new_fasta_file = open(new_fasta_file_name, "w+")
-	new_fasta_file.write(sequences)
+	
+	for line in sequences:
+ 		 new_fasta_file.write("%s" % line)		
 		
 	for gi in gi_final:
 		genbank_raw = library.genbank.fetchseq(gi)
-		fasta_entry = str(">gi|" + gi + "|" + (genbank_raw.description) + "\n" + (genbank_raw.seq) + "\n")  ##replace with a join for speed and to make more concise		
+		fasta_entry = str("".join([">gi|", gi, "\n", str(genbank_raw.seq), "\n"]))		
 		new_fasta_file.write(fasta_entry)
 
 	new_fasta_file.close()
 	print "Done."
+	new_seqs = new_seqs + len(gi_final);
+	return (new_seqs, orig_seqs);
 
 ### Program Code ###
 
 fasta_files = glob.glob("fasta/*.FASTA") # get a list of all fasta files in /fasta
 blast_files = glob.glob("blast-temp/*.xml") # get a list of all fasta files in /fasta
 
-count = 0
+count = 0;
+
+
 for f in fasta_files:
+	
 	## process each file by calling fasta_process
 	fasta_process(f, blast_files[count])
-	print "FASTA file %s successfully expanded." % f
+	print "FASTA file %s successfully merged." % f
 	count = count + 1
+	total_new_seqs = new_seqs + total_new_seqs;
+	total_orig_seqs = orig_seqs;
 
-
-print "Done."
+print "Complete."
 print "Adding additional records to FASTA complete."
-print "New FASTA files can be found in /fasta/expanded-fasta directory."
-
+print "New FASTA files can be found in /fasta/expanded-fasta directory.\n\n\n"
+print "############### SNAPSHOT STATS ###############\n"
+print "Total Files Processed: %s" %len(fasta_files)
+print "Total Original Sequences: %s" % (total_orig_seqs/2)
+print "Total Additional Sequences Included: %s" % total_new_seqs
+print "\n############### SNAPSHOT STATS ###############"
