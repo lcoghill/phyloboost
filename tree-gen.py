4bc3e3fb387eb4dae90c43be06e8171cc642b284
Lyndon Coghill <lcoghill@fieldmuseum.org>
2014-02-08 23:09:03 -0600

    Added notes on RAxML issues. Also correct missing output file.

--- tree-gen.py
+++ tree-gen.py
@@ -3,11 +3,12 @@ from Bio.Phylo.Applications import RaxmlCommandline
 from StringIO import StringIO
 from Bio import AlignIO
 
-
+## Need to add feature to iterate through all files in the alignment directory.
+## Also need to investigate the ability to control where the RAxML output is saved.
 input_handle = open("alignments/testfile.align", "rU")
- 
+output_handle = open("alignments/testfile.align", "w+) 
 alignments = AlignIO.read(input_handle, "clustal")
-#AlignIO.write(alignments, output_handle, "phylip-relaxed")
+AlignIO.write(alignments, output_handle, "phylip-relaxed")
 
 
 ## due to the -n limitation of no "/" characters, must and pass it as an argument?
