e269880c0fcc924bdae988c76b5f49b62d37e5da
Lyndon Coghill <lcoghill@fieldmuseum.org>
2014-02-11 16:38:07 -0600

    Changed blast call to work with local blast+ installation instead of over the Internet.

--- blast-search.py
+++ blast-search.py
@@ -6,6 +6,20 @@ import logging
 import MySQLdb
 from Bio import Entrez, SeqIO, SearchIO
 from Bio.Blast import NCBIWWW, NCBIXML
+from Bio.Blast.Applications import NcbiblastnCommandline
+
+## Retreive NCBI-BLAST NT Database location from Config file
+config = [];
+db_params = open('private/config', 'r')
+
+for line in db_params:
+    line = line.rstrip('\n')
+    line = line.split("=", 1)
+    config.append (line[1])
+
+blast_db = config[4]
+
+
 
 print "\n\nGetting a list of FASTA files..."
 fasta_files = glob.glob("fasta/*.FASTA") # get a list of all fasta files in /fasta
@@ -16,7 +30,7 @@ print "%s files successfully found.\n" %file_count
 if file_count > 2:
 	print "WARNING:\n"
 	print "There are %s FASTA files to be processed." %file_count
-	print "This process can take quite a long time and requires a presistant Internet connection...\n\n"
+	print "This process can take quite a long time...\n\n"
 
 ### Start processing BLAST searches if the user agrees
 count = 0
@@ -30,15 +44,12 @@ for f in fasta_files:
 		fasta_file.close()
 		print "Starting BLASTN Search against %s" %f[6:]
 		## Blast using the FASTA set, and assing the XML string result to result_handle
-		result_handle = NCBIWWW.qblast("blastn", "nt", sequences, expect=8.0)
+		blast_file_id = "".join(["blast-results/", f[6:-6], ".xml"])
+		blastn_cline = NcbiblastnCommandline(query=f, db=blast_db, evalue=10, outfmt=5, out=blast_file_id)
 		print "Done."
 		print "Writing results to a BLAST.XML file..."
 		## write the XML string to a file for later parsing
-		blast_file_id = "".join(["blast-temp/", f[6:-6], ".xml"])
-		save_result = open(blast_file_id, "w")
-		save_result.write(result_handle.read())
-		save_result.close()
-		result_handle.close()
+		stdout, stderr = blastn_cline()
 		print "Step %s / %s complete." % (count + 1, file_count)
 		count = count + 1
 
