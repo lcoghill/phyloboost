c5ebf966f6b200f96fde6a5686f32dbbb31fb983
Lyndon Coghill <lcoghill@fieldmuseum.org>
2014-04-18 22:34:15 -0500

    Basic cleanup and code reformatting.

--- fasta-clean.py
+++ fasta-clean.py
@@ -1,43 +1,46 @@
-from StringIO import StringIO
 from Bio import SeqIO
-from Bio import AlignIO
 import glob
 
-##get list of all new fasta files
+## get list of all new fasta files
 
 print "\n\nGetting a list of FASTA files..."
-fasta_files = glob.glob("output/expanded-fasta/*.fasta") # get a list of all fasta files in /fasta
+fasta_files = glob.glob("output/expanded-fasta/*.fasta")  # get a list of all fasta files in /fasta
 file_count = len(fasta_files)
-print "%s files successful found.\n" %file_count
+print "%s files successful found.\n" % file_count
 
+bad_char_list = []  # just used for bug tracking.
 
-bad_char_list = [] ## just used for bug tracking.
+for f in fasta_files:  # iterate through all the files in the directory
+    print "\nChecking FASTA file %s for non-nucleotide characters..." % f
+    clean_seq_list = []  # build a new list of good sequences
+    bad_seq_count = 0  # keep track of how many sequences are discarded
+    tracker = 0  # track if a sequence is bad or not.
+    for seq_record in SeqIO.parse(f, "fasta"):  # for each sequence
 
-for f in fasta_files: #iterate through all the files in the directory
-	print "\nChecking FASTA file %s for non-nucleotide characters..." %f
-	clean_seq_list = [] #build a new list of good sequences
-	bad_seq_count = 0 #keep track of how many sequences are discarded
-	tracker = 0 #track if a sequence is bad or not.
-	for seq_record in SeqIO.parse(f, "fasta"): #for each sequence
-		
-		dna = seq_record.seq #assign it to a string
-		
-		for char in dna: #for each character in the sequence
-			if char not in ('A','T','C','G','N','U','R','Y','K','M','S','W','B','V','H','D','X','a','t','c','g','n','u','r','y','k','m','s','w','b','v','h','d','x'): #if its not an accepted base or ambiguity code
-				bad_char_list.append(char) #add it to the bad list for bug tracking
-				tracker = 1 # set the bad / good tracker to bad
-	
-		if tracker == 1: #if bad sequence
-			bad_seq_count = bad_seq_count + 1 #increment bad counter
-		elif tracker == 0: #if good sequence
-			clean_seq_list.append(seq_record) #add the record to the good list
-		tracker = 0 #after done iterating through a sequence, reset tracker for the next sequence
-	print "Complete."
-	if bad_seq_count == 0:
-		print "There were no non-nucleotide characters in FASTA file %s." % f
-	else:
-		print "There were %s non-nucleotide containing entries found in the FASTA file. Those entries have been removed." % bad_seq_count
-	
-	SeqIO.write(clean_seq_list, f, "fasta") #after done with all iterations, write the good seq record list to the same file we started with
+        dna = seq_record.seq  # assign it to a string
+
+        for char in dna:  # for each character in the sequence
+            if char not in (
+                    'A', 'T', 'C', 'G', 'N', 'U', 'R', 'Y', 'K', 'M', 'S', 'W', 'B', 'V', 'H', 'D', 'X', 'a', 't', 'c',
+                    'g',
+                    'n', 'u', 'r', 'y', 'k', 'm', 's', 'w', 'b', 'v', 'h', 'd',
+                    'x'):  # if its not an accepted base or ambiguity code
+                bad_char_list.append(char)  # add it to the bad list for bug tracking
+                tracker = 1  # set the bad / good tracker to bad
+
+        if tracker == 1:  # if bad sequence
+            bad_seq_count += 1  # increment bad counter
+        elif tracker == 0:  # if good sequence
+            clean_seq_list.append(seq_record)  # add the record to the good list
+        tracker = 0  # after done iterating through a sequence, reset tracker for the next sequence
+    print "Complete."
+    if bad_seq_count == 0:
+        print "There were no non-nucleotide characters in FASTA file %s." % f
+    else:
+        print
+        'There were %s non-nucleotide containing entries found in the FASTA file. Those entries have been removed.' % bad_seq_count
+
+    SeqIO.write(clean_seq_list, f,
+                "fasta")  # after all iterations, write the good seq record list to the same file we started with
 
 print "\nFASTA clean complete."
--- fasta-csv.py
+++ fasta-csv.py
@@ -1,46 +1,41 @@
-import os
 from Bio import SeqIO
 
 
-
-###### FUNCTIONS  #######
-        
 def parse_config():
-    config_params = [];
-    config = open('private/config', 'r') 
+    config_params = []
+    config = open('private/config', 'r')
     for line in (line for line in config if not line.startswith('###')):
         line = line.rstrip('\n')
         line = line.split("=")
-        config_params.append (line[1])
+        config_params.append(line[1])
 
     return config_params
 
-#####
 
 ## Gather import configuration information from the config file
-params = parse_config() # retreive the params from the config file
+params = parse_config()  # retrieve the params from the config file
 
 
 
 ## Gather import configuration information from the config file
-params = parse_config() # retreive the params from the config file
 print "Please enter the name of your nucleotide file from Genbank."
 print "The script will look in %s (specified input location) for the file." % params[7]
 file_name = raw_input("Filename: ")
-full_file_name = "".join([params[7],file_name])
+full_file_name = "".join([params[7], file_name])
 print "Opening %s" % file_name
 
-## Iterate through each record in the fasta file, parse out the important information and write that information to a csv file
-f = open(full_file_name,'r+') # nt fasta file
-csv_file = "".join([params[6],file_name,".csv"])
-csv = open(csv_file, "w+") #output csv file
+## Iterate through each record, parse out the important information and write that information to a csv file
+f = open(full_file_name, 'r+')  # nt fasta file
+csv_file = "".join([params[6], file_name, ".csv"])
+csv = open(csv_file, "w+")  # output csv file
 
 count = 0
-for record in SeqIO.parse(full_file_name, "fasta") :
+for record in SeqIO.parse(full_file_name, "fasta"):
     seq_list = record.description.split("|")
-    csv_string = '%s, %s, %s, %s, %s' % (seq_list[1], seq_list[3], len(record.seq), seq_list[4][1:], record.seq) #convert record to string
+    ## convert record to string
+    csv_string = '%s, %s, %s, %s, %s' % (seq_list[1], seq_list[3], len(record.seq), seq_list[4][1:], record.seq)
     csv.write("%s\n" % csv_string)
-    print "Record %s converted to CSV format" %count
+    print "Record %s converted to CSV format" % count
     count = count + 1
 f.close()
 csv.close()
