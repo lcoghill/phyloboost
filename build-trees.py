8b79933630ada9a8d885a0ad18335a0181fb4829
Lyndon Coghill <lcoghill@fieldmuseum.org>
2014-07-11 09:35:57 -0500

    Fixed bugs in generic variable names.

--- build-trees.py
+++ build-trees.py
@@ -7,9 +7,9 @@ import os
 import re
 
 ## locate trees already generated
-def done_trees(trees.directory):
+def done_trees(trees_directory):
     print "Getting a list of trees already built..."
-    tree_files = glob.glob(trees.directory) # get a list of all files in trees.directory
+    tree_files = glob.glob(trees_directory) # get a list of all files in trees.directory
     complete_trees = []
     for t in tree_files:
         name = t[28:]
@@ -19,13 +19,13 @@ def done_trees(trees.directory):
 
 
 ## write tree files to single output tree file
-def write_trees(tree.outfile, trees.directory):
+def write_trees(tree_outfile, trees_directory):
 
-    tree_files = glob.glob("".join([trees.directory,"RAxML_bestTree.*"]))
-    tree_file = open(tree.outfile, 'w+')
+    tree_files = glob.glob("".join([trees_directory,"RAxML_bestTree.*"]))
+    tree_file = open(tree_outfile, 'w+')
     for f in tree_files:
         print "-"*50
-        print "Writing trees to file $s" %tree.outfile
+        print "Writing trees to file $s" %tree_outfile
         newick_string = open(f)
         record = "".join([name,"\t",newick_string,"\n"])
         tree_file.write(record) 
@@ -34,7 +34,7 @@ def write_trees(tree.outfile, trees.directory):
 
 
 ## delete all tree files when done?
-def delete_trees(keep_all_trees, trees.directory):
+def delete_trees(keep_all_trees, trees_directory):
 
     if keep_all_trees == 0:
         # delete all files in trees directory here
@@ -51,26 +51,25 @@ def delete_trees(keep_all_trees, trees.directory):
 
 
 ### Some important variables                        ###
-tree.outfile = "trees.out"                          # final, combined output file name
-trees.directory = "/trees"                          # directory where tree files should be saved
+tree_outfile = "trees.out"                          # final, combined output file name
+trees_directory = "trees"                          # directory where tree files should be saved
 keep_all_trees = 1                                  # flag whether to keep all tree output files when the process is done. (0 = Delete Everything, 1 = Keep Everything)
-alignment.files = "/alignments"                     # location where all alignment files are stored. (Must be in Phylip format with .phy extension)
-num.threads = 1                                     # number of threads to use in RAxML. Don't use more than physical cores in on the computer.
-num.replicates = 1                                  # number of replicates for RAxML
-evo.model = "GTRCAT"                                # model of evolution used for RAxML
+alignment_files = "alignments"                     # location where all alignment files are stored. (Must be in Phylip format with .phy extension)
+num_threads = 4                                     # number of threads to use in RAxML. Don't use more than physical cores in on the computer.
+num_replicates = 1                                  # number of replicates for RAxML
+evo_model = "GTRCAT"                                # model of evolution used for RAxML
 
 ###
 
 
 
 
-
-complete_trees = done_trees(trees.directory) # get a list of the trees already built, by comparing files in working dir vs those in alignment.files
-alignments = glob.glob("".join([alignment.files,".phy"])) # get a list of all phylip files in /alignment
+complete_trees = done_trees(trees_directory) # get a list of the trees already built, by comparing files in working dir vs those in alignment.files
+alignments = glob.glob(alignment_files + '/*.fasta') # get a list of all phylip files in /alignment
 alignment_names = [] #names of alignment files without directory or suffix
 print "\n\nGetting a list of alignment files..."
 for a in alignments:
-    match = re.match(r"^.*\/(.*).phy.*$",a)
+    match = re.match(r"^.*\/(.*).fasta.*$",a)
     name = match.group(1)
     alignment_names.append(name)
 
@@ -83,15 +82,14 @@ print "-"*50+"\n"
 ## build the trees using RAxML
 count = 0 ## status counter
 for f in files_to_use:
-    infile = "".join([alignments,f,'.phy'])
+    infile = "".join([f,'.fasta'])
     print "Builing tree for %s..." %f
-    raxml_cline = RaxmlCommandline(sequences=infile, model=evo.model, name=f, working_dir=trees.directory, threads=num.threads, num_replicates=num.replicates) ## raxml wrapper call. Assumes the use of the pthreads version of raxml 8.0 or above
+    raxml_cline = RaxmlCommandline(sequences=infile, model=evo_model, name=f, working_dir=trees_directory, threads=num_threads, num_replicates=num_replicates) ## raxml wrapper call. Assumes the use of the pthreads version of raxml 8.0 or above
     stdout, stderr = raxml_cline()
     print "Tree %s / %s Complete." %(count, len(files_to_use))
     count += 1 
 
-write_trees(tree.outfile) # write all "best" trees to a single file in newick format
-delete_trees(keep_all_trees, trees.directory) # delete or keep all tree files
-tree_file.close()
+write_trees(tree_outfile, trees_directory) # write all "best" trees to a single file in newick format
+delete_trees(keep_all_trees, trees_directory) # delete or keep all tree files
 print "-"*50
 print "Tree file generation complete."
