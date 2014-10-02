b2a83426f14fbbad42cf6d8e6949edc2e625bce8
Lyndon Coghill <lcoghill@fieldmuseum.org>
2014-10-02 15:33:11 -0500

    Added DAL structure for Phyloboost.

--- phyloboost/models/db.py
+++ phyloboost/models/db.py
@@ -1,84 +1,84 @@
-# -*- coding: utf-8 -*-
+import datetime, socket, os, sys
+import MySQLdb
+from gluon.dal import MySQLAdapter
+from ConfigParser import SafeConfigParser
+from gluon.tools import *
 
-#########################################################################
-## This scaffolding model makes your app work on Google App Engine too
-## File is released under public domain and you can use without limitations
-#########################################################################
+MySQLAdapter.driver = MySQLdb
+defaults = dict(host="localhost", user="guest", password="guest", dbname="phyloboost")
+conf = SafeConfigParser(defaults)
+user = password = dbname = host = ''
 
-## if SSL/HTTPS is properly configured and you want all HTTP requests to
-## be redirected to HTTPS, uncomment the line below:
-# request.requires_https()
 
-if not request.env.web2py_runtime_gae:
-    ## if NOT running on Google App Engine use SQLite or other DB
-    db = DAL('sqlite://storage.sqlite',pool_size=1,check_reserved=['all'])
+if os.path.isfile("applications/%s/private/localconfig" % request.application):
+    conf.read("applications/%s/private/localconfig" % request.application)
+    host = conf.get("db", "host")
+    user = conf.get("db", "user")
+    password = conf.get("db", "password")
+    dbname = conf.get("db", "dbname")
+
 else:
-    ## connect to Google BigTable (optional 'google:datastore://namespace')
-    db = DAL('google:datastore+ndb')
-    ## store sessions and tickets there
-    session.connect(request, response, db=db)
-    ## or store session in Memcache, Redis, etc.
-    ## from gluon.contrib.memdb import MEMDB
-    ## from google.appengine.api.memcache import Client
-    ## session.connect(request, response, db = MEMDB(Client()))
+    conf.read("applications/%s/private/config" % request.application)
+    host = conf.get("db", "host")
+    user = conf.get("db", "user")
+    password = conf.get("db", "password")
+    dbname = conf.get("db", "dbname")
 
-## by default give a view/generic.extension to all actions from localhost
-## none otherwise. a pattern can be 'controller/function.extension'
-response.generic_patterns = ['*'] if request.is_local else []
-## (optional) optimize handling of static files
-# response.optimize_css = 'concat,minify,inline'
-# response.optimize_js = 'concat,minify,inline'
-## (optional) static assets folder versioning
-# response.static_version = '0.0.0'
-#########################################################################
-## Here is sample code if you need for
-## - email capabilities
-## - authentication (registration, login, logout, ... )
-## - authorization (role based authorization)
-## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
-## - old style crud actions
-## (more options discussed in gluon/tools.py)
-#########################################################################
+db = DAL("mysql://%s:%s@%s/%s" % (user, password, host, dbname), migrate=False, migrate_enabled=False )
 
-from gluon.tools import Auth, Crud, Service, PluginManager, prettydate
-auth = Auth(db)
-crud, service, plugins = Crud(db), Service(), PluginManager()
+## mail = Mail()                    # mailer
+auth = Auth(globals(),db)           # authentication/authorization
+crud = Crud(globals(),db)           # for CRUD helpers using auth
+service = Service(globals())        # for json, xml, jsonrpc, xmlrpc, amfrpc
+plugins = PluginManager()
+host = socket.gethostname()
+crud.settings.auth = None           # =auth to enforce authorization on crud
+auth.define_tables( migrate=False )                           # creates all needed tables
 
-## create all tables needed by auth if not custom tables
-auth.define_tables(username=False, signature=False)
+try:
+    host = conf.get("hosting", "hostdomain")
+except:
+    host = "localhost:8000"
 
-## configure email
-mail = auth.settings.mailer
-mail.settings.server = 'logging' or 'smtp.gmail.com:587'
-mail.settings.sender = 'you@gmail.com'
-mail.settings.login = 'username:password'
+db.define_table('convex_subtrees', 
+    Field('id', 'id'),                       
+    Field('ci', 'integer'),                  
+    Field('date', 'date'),                   
+    Field('tree', 'text'),                   
+    Field('method', 'string'),               
+    )
 
-## configure auth policy
-auth.settings.registration_requires_verification = False
-auth.settings.registration_requires_approval = False
-auth.settings.reset_password_requires_verification = True
+db.define_table('sequences', 
+    Field('id', 'id'),                       
+    Field('ti', 'integer'),                  
+    Field('tax_name', 'string'),             
+    Field('phylota_ci', 'integer'),          
+    Field('sequence', 'text'),               
+    Field('genome', 'string'),               
+    Field('gene', 'text'),                   
+    Field('accession', 'string'),            
+    Field('gi', 'integer'),                  
+    Field('pb_version', 'integer'),          
+    Field('mol_type', 'text'),               
+    Field('genbank_div', 'string'),          
+    Field('description', 'text'),            
+    )
 
-## if you need to use OpenID, Facebook, MySpace, Twitter, Linkedin, etc.
-## register with janrain.com, write your domain:api_key in private/janrain.key
-from gluon.contrib.login_methods.janrain_account import use_janrain
-use_janrain(auth, filename='private/janrain.key')
+db.define_table('trees', 
+    Field('id', 'id'),                       
+    Field('ci', 'integer'),                  
+    Field('ti', 'integer'),                  
+    Field('tree', 'text'),                   
+    Field('method', 'string'),               
+    Field('date', 'date'),                   
+    )
 
-#########################################################################
-## Define your tables below (or better in another model file) for example
-##
-## >>> db.define_table('mytable',Field('myfield','string'))
-##
-## Fields can be 'string','text','password','integer','double','boolean'
-##       'date','time','datetime','blob','upload', 'reference TABLENAME'
-## There is an implicit 'id integer autoincrement' field
-## Consult manual for more options, validators, etc.
-##
-## More API examples for controllers:
-##
-## >>> db.mytable.insert(myfield='value')
-## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
-## >>> for row in rows: print row.id, row.myfield
-#########################################################################
-
-## after defining tables, uncomment below to enable auditing
-# auth.enable_record_versioning(db)
+db.define_table('versions', 
+    Field('id', 'id'),                       
+    Field('version', 'integer'),             
+    Field('creation_date', 'date'),          
+    Field('ncbi_taxonomy', 'date'),          
+    Field('phylota_version', 'integer'),     
+    Field('genbank_version', 'integer'),     
+    Field('repbase_version', 'integer'),     
+    )
