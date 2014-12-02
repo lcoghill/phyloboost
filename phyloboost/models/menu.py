961335b2fdcb2fabe3e8624f7e714f84869b31af
Lyndon Coghill <lcoghill@fieldmuseum.org>
2014-12-02 12:27:54 -0600

    Added basic "trees" submenu structure.

--- phyloboost/models/menu.py
+++ phyloboost/models/menu.py
@@ -5,15 +5,15 @@
 ## Customize your APP title, subtitle and menus here
 #########################################################################
 
-response.logo = A(B('web',SPAN(2),'py'),XML('&trade;&nbsp;'),
-                  _class="brand",_href="http://www.web2py.com/")
+#response.logo = A(B('web',SPAN(2),'py'),XML('&trade;&nbsp;'),
+#                  _class="brand",_href="http://www.web2py.com/")
 response.title = request.application.replace('_',' ').title()
 response.subtitle = ''
 
 ## read more at http://dev.w3.org/html5/markup/meta.name.html
-response.meta.author = 'Your Name <you@example.com>'
-response.meta.keywords = 'web2py, python, framework'
-response.meta.generator = 'Web2py Web Framework'
+response.meta.author = 'Lyndon M Coghill <lcoghill@fieldmuseum.org>'
+response.meta.keywords = ''
+response.meta.generator = ''
 
 ## your http://google.com/analytics id
 response.google_analytics_id = None
@@ -21,25 +21,13 @@ response.google_analytics_id = None
 #########################################################################
 ## this is the main application menu add/remove items as required
 #########################################################################
-
+app = request.application
+ctr = request.controller
 response.menu = [
-    (T('Home'), False, URL('default', 'index'), [])
-]
-
-DEVELOPMENT_MENU = True
-
-#########################################################################
-## provide shortcuts for development. remove in production
-#########################################################################
-
-def _():
-    # shortcuts
-    app = request.application
-    ctr = request.controller
-    # useful links to internal and external resources
-    response.menu += [
-        (SPAN('Sequences'), 'sequences', False, [   
-        (T('Browse All Sequences'), False, URL('sequences', 'view')),
+    (T('Home'), False, URL('default', 'index'), []),
+    (T('About'), False, URL('about', 'index'), []),
+    (SPAN('Sequences'), 'sequences', False, [   
+        (T('Browse All Sequences'), False, URL('sequences', 'index')),
         (T('Search Sequences'), False, URL('sequences', 'search'))]),
         
         (SPAN('Clusters'), False, 'clusters', [   
@@ -47,10 +35,10 @@ def _():
         (T('Search Clusters'), False, URL('admin', 'default', 'design/%s' % app))]),
         
         (SPAN('Trees'), False, 'trees', [   
-        (T('Browse All Unrooted Trees'), False, URL('admin', 'default', 'site')),
-        (T('Browse All Convex Subtrees'), False, URL('admin', 'default', 'design/%s' % app)),
-        (T('Search Unrooted Subtrees'), False, URL('admin', 'default', 'design/%s' % app)),
-        (T('Search Convex Subtrees'), False, URL('admin', 'default', 'design/%s' % app))])]
-if DEVELOPMENT_MENU: _()
+        (T('Browse All Unrooted Trees'), False, URL('trees', 'index')),
+        (T('Browse All Convex Subtrees'), False, URL('convexsubtrees', 'index')),
+        (T('Search Unrooted Subtrees'), False, URL('trees', 'search')),
+        (T('Search Convex Subtrees'), False, URL('convexsubtrees', 'search'))])]
 
-if "auth" in locals(): auth.wikimenu() 
+
+#if "auth" in locals(): auth.wikimenu()
