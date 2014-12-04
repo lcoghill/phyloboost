573d0d9942ec5be0e1d5e374ba717eb64361ca0e
Lyndon Coghill <lcoghill@fieldmuseum.org>
2014-12-04 15:00:59 -0600

    Fleshed out "about" section. Added methods, bibliography and credits pages.

--- phyloboost/controllers/about.py
+++ phyloboost/controllers/about.py
@@ -2,6 +2,40 @@ def index():
 
     query = "".join(["SELECT * FROM versions;"])
     all_rows = db.executesql(query)
+
+    if str(request.args(0)) :
+    	for r in all_rows :
+    		if r[1] == str(request.args(0)) :
+    			row = r
+    else :
+    	row = all_rows[-1]
+
+    return dict(row=row, all_rows=all_rows)
+
+def methods():
+
+    query = "".join(["SELECT * FROM versions;"])
+    all_rows = db.executesql(query)
+    for r in all_rows :
+    	if r[3] is 1 :
+    		row = r
+
+    return dict(row=row, all_rows=all_rows)
+
+def bibliography():
+
+    query = "".join(["SELECT * FROM versions;"])
+    all_rows = db.executesql(query)
+    for r in all_rows :
+    	if r[3] is 1 :
+    		row = r
+
+    return dict(row=row, all_rows=all_rows)
+
+def credits():
+
+    query = "".join(["SELECT * FROM versions;"])
+    all_rows = db.executesql(query)
     for r in all_rows :
     	if r[3] is 1 :
     		row = r
