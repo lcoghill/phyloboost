e98d9ad9069efb116e316029d42423923235f8cd
Lyndon Coghill <lcoghill@fieldmuseum.org>
2015-05-27 16:59:09 -0500

    algorithm rework for calculating distances

--- pipeline/mad-filter.py
+++ pipeline/mad-filter.py
@@ -1,6 +1,4 @@
 from ivy import treegraph as tg
-from collections import Counter
-import graph_tool.all as gt
 from Bio import SeqIO
 import numpy as np
 import glob
@@ -13,16 +11,21 @@ def mad(data) :
     ## the median and each data point.
     return np.median(np.abs(data - np.median(data)))
 
-def flag_outliers(data, keys, cutoff) :
+def flag_outliers(distances, cutoff) :
     ## flag any sequence as an outlier that is more than 'cutoff' standard deviations
     ## away from median distance of the rest of the points.
-    
+    keys = []
+    vals = []
+    for key, val in distances.items() :
+        keys.append(key)
+        vals.append(val)
+
     outliers = []
     ## attempts to adjusts for the MAD=0 problem.
-    if len(set(data)) > 1 :
-        mad_res = mad(data)
+    if len(set(vals)) > 1 :
+        mad_res = mad(vals)
         if mad_res > 0 :
-            mad_data = np.abs(data - np.median(data)) / mad_res
+            mad_data = np.abs(vals - np.median(vals)) / mad_res
         else :
             mad_data = []    
         
@@ -48,6 +51,49 @@ def fetch_gis(cluster, bad_tis) :
             gis_with_tis[gi] = ti
 
     return gis_with_tis, bad_tis
+
+def calc_distances(gis_with_tis, g) :
+    
+    ## gathers the rootpath for each ti in a cluster
+    ## then looks for the intersection for each rootpath with all other rootpaths in a cluster
+    ## calculates the number of steps to each TI in the result of the intersection
+    ## keeps the shortest distance
+    
+    gis = []
+    tis = []
+    for key, val in gis_with_tis.items() :
+        tis.append(val)
+        gis.append(key)
+
+    distances = {}
+    rootpaths = []
+    for t in tis :
+        rootpaths.append(tg.taxid_rootpath(g, t))
+
+    for i, r in enumerate(rootpaths) :
+        ccas = []
+        matches = []
+        for r2 in rootpaths :
+            if r != r2 :
+                paths = []
+                paths.append(r)
+                paths.append(r2)
+                tcca = tg.rootpath_mrca(paths)
+                matches.append(tcca)
+       
+        if len(set(matches)) > 1 :
+            dist_set = []
+            for m in set(matches) :
+                dist_set.append(r.index(m))
+            dist = min(dist_set)
+            cca = r[dist_set.index(dist)]
+        else :
+           cca = matches[0]
+           dist = r.index(cca)
+        
+        distances[gis[i]] = dist
+
+    return distances
 
 def write_seqs(cluster, outliers, out_cluster_dir) :
     sequences = list(SeqIO.parse(open(cluster), 'fasta'))
@@ -75,7 +121,7 @@ def log_outliers(c, outlier_seqs, log_file) :
         rec_id = s.id.split("_")
         gi = rec_id[0][2:]
         ti = rec_id[1][2:]
-        out_handle.write(",".join([clust, gi, ti, str(mcp[0][0]), str(s.seq), "\n"]))
+        out_handle.write(",".join([clust, gi, ti, str(s.seq), "\n"]))
 
     out_handle.close()
 
@@ -103,8 +149,9 @@ total = len(clusters)
 good_seq_count = 0
 bad_seq_count = 0
 out_handle = open(log_file, 'a')
-out_handle.write(",".join(["Cluster", "GI", "TI", "MCP", "Sequence", "\n"]))
+out_handle.write(",".join(["Cluster", "GI", "TI", "Sequence", "\n"]))
 out_handle.close()
+
 
 
 if __name__ == '__main__' :
@@ -119,38 +166,13 @@ if __name__ == '__main__' :
         ## fetch the parent of each gi
         ## reset the graph to directed to use tg.taxid_rootpath
         g.set_directed(True)
-        for key, val in gis_with_tis.items() :
-        ## find the parent of each TI in the graph
-        ## find the most common parent for the entire cluster
-            cas.append(tg.taxid_rootpath(g, val)[1])
-                
-        ## count occurences to find the most common parent
-        count = Counter(cas)
-        mcp = count.most_common(1)
+
+        ## calculate the distance for each TI in a cluster to the closest shared ancestor with any
+        ## other TI in a cluster.
+        distances = calc_distances(gis_with_tis, g)
         
-
-        ## have to remove the direction of the graph in order to get a distance
-        ## from one tip to another. Otherwise in directed graphs, shortest_distance
-        ## can only be calculated in the same direction as the graph
-        g.set_directed(False)
-
-        ## calculate the shortest distance from each tip, to the most-common-parent
-        distances = {}
-        for key, val in gis_with_tis.items() :
-            source = g.taxid_vertex[val]
-            target = g.taxid_vertex[mcp[0][0]]
-            dist = gt.shortest_distance(g, source, target)
-            distances[key] = dist
-
-        ## book-keeping 
-        dst = []
-        keys = []
-        for key, val in distances.items() :
-            keys.append(key)
-            dst.append(val)
-
         ## flag outliers that are more than "cutoff" SDs away from group median
-        outliers = flag_outliers(dst, keys, cutoff)
+        outliers = flag_outliers(distances, cutoff)
         
         ## write non-outlier sequences to new fasta file, and log outlier sequences for further review
         good_count, outlier_seqs = write_seqs(c, outliers, out_cluster_dir)
@@ -160,7 +182,7 @@ if __name__ == '__main__' :
         good_seq_count = good_seq_count + good_count
         bad_seq_count = bad_seq_count + len(outliers)
         ## write the outlier sequences
-        log_outliers(outlier_seqs, log_file)
+        log_outliers(c, outlier_seqs, log_file)
 
         status += 1
         ## dump missing ti list to file to check / remove from clusters
